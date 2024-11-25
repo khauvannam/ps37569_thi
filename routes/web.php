9cb9bf23e032797d3a6563731d7508ddcb81dd18
khauvannam <nkhauvan@gmail.com>
2024-11-25 22:48:36 +0700

    hello

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+
 require __DIR__ . '/auth.php';
